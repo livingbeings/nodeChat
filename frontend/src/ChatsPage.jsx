36104c76063da53f4f37961ffbbfaa8248c00787
livingbeings <alexsanjayaprasetia@gmail.com>
2023-09-03 23:04:43 +0800

    created Chat Page using chat-engine-advanced

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,16 @@
-const ChatsPage = () => {
-  return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    'c264c251-9188-40be-a280-2645e9555a68',
+    props.user.username,
+    props.user.secret
+  )
+  return (
+    <div style={{ height: '100vh' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+    </div>
+  )
 }
 export default ChatsPage;
